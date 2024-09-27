3fa8a23e1a5d7b287f47978d2ef05c41fbe490f7
Anas Abbas <anasabbasmalik88@gmail.com>
2024-09-27 11:49:36 +0500

    Getting finished with the skeleton of ChatApp

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { JsonPipe } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,JsonPipe],  
+  imports: [RouterOutlet,NgFor],  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  
 })
